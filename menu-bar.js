14fa4cf2c26540071efc4685abd18951a7d6d175
Fsher07 <furkansalimm@gmail.com>
2022-03-01 22:18:38 +0300

    Create js function to popup window

--- menu-bar.js
+++ menu-bar.js
@@ -1,14 +1,22 @@
 const menubar = document.querySelector('.menu-bar');
 const listItems = document.querySelectorAll('.navbarItems'); // list of all the items in the menu bar (navbarItems)
-const closeicon = document.querySelector('#hambuger');
+const closeicon = document.querySelector('#closeIcon');
 const openicon = document.querySelector ('#openIcon');
 const hamburgerMenu = document.querySelector('.header__nav-container__mobile_menu');
+const closingMenu = document.querySelector('#hamburger');
 
 function toggleMenu() {
-  
-    menubar.style.display = 'block';
-
+  if (menubar.classList.contains('open-class')) {
+    menubar.classList.remove('open-class');
+    closeicon.style.display = 'none';
+    openicon.style.display = 'block';
+} else {
+    menubar.classList.add('open-class');
+    closeicon.style.display = 'block';
+    openicon.style.display = 'none';
+}
 }
 
 
 hamburgerMenu.addEventListener("click", toggleMenu);
+closingMenu.addEventListener("click", toggleMenu);
