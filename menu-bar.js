f42048f4c7c915a6ce9d1721e3014d3582ea5e67
lyamz <davidlyangenda@gmail.com>
2022-03-01 20:41:29 +0200

    Add hambuger menu

--- menu-bar.js
+++ menu-bar.js
@@ -1,2 +1,4 @@
 const menubar = document.querySelector('.menu-bar');
 const listItems = document.querySelectorAll('.navbarItems'); // list of all the items in the menu bar (navbarItems)
+const closeicon = document.querySelector('#hambuger');
+const openicon = document.querySelector ('.header__nav-container__mobile_menu');
