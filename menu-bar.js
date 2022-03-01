be9da1648b91b36f31ebea4f2237ef14b275503e
Fsher07 <furkansalimm@gmail.com>
2022-03-01 22:04:31 +0300

    Update script file

--- menu-bar.js
+++ menu-bar.js
@@ -1,4 +1,14 @@
 const menubar = document.querySelector('.menu-bar');
 const listItems = document.querySelectorAll('.navbarItems'); // list of all the items in the menu bar (navbarItems)
 const closeicon = document.querySelector('#hambuger');
-const openicon = document.querySelector ('.header__nav-container__mobile_menu');
+const openicon = document.querySelector ('#openIcon');
+const hamburgerMenu = document.querySelector('.header__nav-container__mobile_menu');
+
+function toggleMenu() {
+  
+    menubar.style.display = 'block';
+
+}
+
+
+hamburgerMenu.addEventListener("click", toggleMenu);
